abd67199da8001c7c3bc020c7102900f3884b89b
jhe2426 <jhe2426@naver.com>
2026-05-11 19:32:26 +0900

    JDBC 개발 - 등록

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,82 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/*
+    JDBC - DriverManager 사용
+*/
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+
+        /*
+            아래와 같이 코드를 작성해서 자원을 반납하려고 할 때 발생할 수 있는 예외가 하나가 존재하는데
+            바로 pstmt.close();를 실행하려다가 예외가 발생한 경우이다. 이때에는 Exception이 발생하면 catch문으로 가게 되므로
+            아래의 con.close()가 실행되지 않음 그래서 각 각의 코드를 전부 다 try-catch로 묶어서 위의 코드가 예외가 발생해도
+            밑에 코드는 영향없게 정상적으로 자원이 반납되도록 코드를 작성해야함
+            자원을 반납하는 코드는 모든 sql작성한 뒤에는 전부다 작성해야해서 아래에 메서드로 구현을 해놨음
+            finally {
+
+                pstmt.close();
+                con.close();
+            }
+        */
+
+
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+
+}
