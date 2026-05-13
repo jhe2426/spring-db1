521f0d487eece2c6aba7b3b7eb32a225084e1148
jhe2426 <jhe2426@naver.com>
2026-05-13 22:56:26 +0900

    JDBC 개발 - 수정, 삭제

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +20,7 @@ class MemberRepositoryV0Test {
     void crud() throws SQLException {
 
         // save
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV100", 10000);
         repository.save(member);
 
         // findById
@@ -28,5 +29,21 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}", member.equals(findMember));
         assertThat(findMember).isEqualTo(member);
+
+        // update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        /*
+            이 테스트 코드 메서드는 마지막에 회원을 삭제하는 코드가 작성되어 있기 때문에 테스트가 정상 수행되면,
+            이제부터는 이 테스트를 실행할때 회원의 아이디를 직접 수정을 해줬는데 수정하지도 않고 반복해서 실행할 수 있다.
+            물론 테스트 중간에 오류가 발생해서 삭제 로직을 수행할 수 없다면 테스트를 반복해서 실행할 수 없다.
+                이 문제는 트랜잭션을 활용하면 해결할 수 있다.
+        */
+        // delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
