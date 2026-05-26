44d751a5b119a5eb9ccf520402f0c04b04a749b4
jhe2426 <jhe2426@naver.com>
2026-05-26 17:04:15 +0900

    예외 포함과 스택 트레이스

--- jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -17,6 +17,18 @@ public class UnCheckedAppTest {
                 .isInstanceOf(Exception.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            log.info("ex", e);
+        }
+
+    }
+
     static class Controller {
         Service service = new Service();
 
@@ -47,6 +59,12 @@ public class UnCheckedAppTest {
                 runSQL();
             } catch (SQLException e) {
                 throw new RuntimeSQLException(e);
+                /*
+                    throw new RuntimeSQLException();
+                    이렇게 예외를 포함하지 않으면 기존에 발생한 java.sql.SQLException과 관련된 스택 트레이스를 확인할 수가 없게 된다.
+                    변환한 RuntimeSQLException부터의 예외만 확인할 수 있게 된다. 만약 실제 DB와 연동했다면 DB에서 발생한 예외를 확인할 수 없게
+                    되는 심각한 문제가 발생한다.
+                */
             }
         }
 
@@ -62,6 +80,9 @@ public class UnCheckedAppTest {
     }
 
     static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException() {
+        }
+
         public RuntimeSQLException(Throwable cause) {
             super(cause);
         }
